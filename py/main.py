ab2851306aefb8925669c8762a43a03d2f8b3fee
Adam Klein <adam3207@gmail.com>
2022-09-16 18:37:02 -0700

    More work on backpropogation, including tensor-based indexing

--- py/main.py
+++ py/main.py
@@ -7,16 +7,25 @@ import matplotlib.pyplot as plt
 import time
 
 NUM_STEPS = 60
-LAYER_SIZE = 1024
+LAYER_SIZE = 8
 NUM_RECORD = 16
-NUM_HIDDEN = 8
+NUM_HIDDEN = 2
 
 def main(args):
+
+    # useful for debugging
+    if args.full_print:
+        torch.set_printoptions(profile="full")
+
+    # get the correct device
     device = torch.device("cpu")
     if args.cuda:
         device = torch.device('cuda:0')
+    
+    # init the net
     net = ordinet.Ordinet(LAYER_SIZE, NUM_HIDDEN, device=device)
 
+    inputs = []
     outputs = []
 
     x_on = torch.full([net.layer_size], 1.0, device=device)
@@ -27,11 +36,19 @@ def main(args):
         y = None
         if i == 0:
             y = net.forward(x_on)
+            inputs.append(x_on)
         else:
             y = net.forward(x_off)
+            inputs.append(x_off)
         
-        outputs.append(y.tolist()[:NUM_RECORD])
+        #outputs.append(y.tolist()[:NUM_RECORD])
+        outputs.append(y)
     print("Time:", round((time.time_ns()-start_time)/1000000, 2))
+
+    input_seq = torch.stack(inputs)
+    output_seq = torch.stack(outputs)
+    
+    net.backward(input_seq, output_seq)
 
     plt.plot([[i for _ in range(NUM_RECORD)] for i in range(NUM_STEPS)], outputs)
     plt.savefig('output.png')
@@ -41,6 +58,8 @@ if __name__ == '__main__':
 
     parser.add_argument('--gpu', dest='cuda', action='store_const', const=True, default=False, 
                     help='Whether to use cuda gpu acceleration')
+    parser.add_argument('--full', dest='full_print', action='store_const', const=True, default=False, 
+                    help='Whether to print entire tensors')
 
     args = parser.parse_args()
     main(args)
--- py/ordinet.py
+++ py/ordinet.py
@@ -1,5 +1,5 @@
 
-from unittest.mock import DEFAULT
+from jmespath import search
 import numpy as np
 import math
 import torch
@@ -23,6 +23,7 @@ class Ordinet:
 
         # information about the size of the net
         self.layer_size = layer_size
+        self.num_hidden = num_hidden
         self.num_layers = num_hidden + 2
         self.container_size = (self.num_layers-1, self.layer_size, self.layer_size)
 
@@ -43,7 +44,7 @@ class Ordinet:
         try:
             self.act_func = ACT_FUNCS[act_func.lower()]
         except:
-            raise("Error: invalid activation function name.")
+            raise ValueError("Invalid activation function name.")
         # whether the last activation should have an activation function
         self.last_activates = last_activates # TODO: Put this in backward pass
 
@@ -51,15 +52,17 @@ class Ordinet:
         # mutate gain and gamma values
 
         if gains[index].size() != gammas[index].size():
-            raise("Error: Layer initialization has gain and gamma tensor sizes that do not match.")
+            raise ValueError("Layer initialization has gain and gamma tensor sizes that do not match.")
         if gains[index].dim() != 2:
-            raise("Error: layer initialization should be done on an indexed 2D layer.")
+            raise ValueError("Layer initialization should be done on an indexed 2D layer.")
         n_inputs = gains[index].size(dim=1)
         matrix_size = gains[index].size(dim=0), n_inputs
 
         # He initialization on integral and gain
         integrals = torch.normal(0.0, math.sqrt(2/n_inputs), matrix_size, dtype=self.dtype, device=self.device)
         gains[index] = torch.normal(0.0, math.sqrt(1/n_inputs), matrix_size, dtype=self.dtype, device=self.device) # 1 seems to work better for 60fps
+
+        #integrals = torch.subtract(integrals, gains[index])
 
         # select pole to give the integral
         poles = torch.div(gains[index], integrals)
@@ -74,13 +77,13 @@ class Ordinet:
 
     def forward(self, phi: torch.Tensor):
         if phi.dim() != 1:
-            raise("Error: Input tensor to forward must be 1 dimensional.")
+            raise ValueError("Input tensor to forward must be 1 dimensional.")
         if phi.size(dim=0) != self.layer_size:
-            raise("Error: Input tensor to forward is the wrong size.")
+            raise ValueError("Input tensor to forward is the wrong size.")
         if phi.device != self.device:
-            raise("Error: Input tensor to forward does not match device with Ordinet.")
+            raise ValueError("Input tensor to forward does not match device with Ordinet.")
         if phi.dtype != self.dtype:
-            raise("Error: Input tensor to forwrd does not math dtype with Ordinet.")
+            raise ValueError("Input tensor to forwrd does not math dtype with Ordinet.")
 
         y = phi.clone()
 
@@ -100,28 +103,29 @@ class Ordinet:
 
     def backward(self, x_sequence, loss_gradients, max_tau=DEFAULT_MAX_TAU, d_offset = 0.99):
         if x_sequence.size(dim=0) != loss_gradients.size(dim=0):
-            raise("Error: x_sequence and loss_gradients sizes do not match on temporal dimension")
+            raise ValueError("x_sequence and loss_gradients sizes do not match on temporal dimension")
         if x_sequence.size(dim=1) != self.layer_size:
-            raise("Error: x_sequence has wrong input size.")
+            raise ValueError("x_sequence has wrong input size.")
         if loss_gradients.size(dim=1) != self.layer_size:
-            raise("Error: loss_gradients has wrong output size.")
+            raise ValueError("loss_gradients has wrong output size.")
         if x_sequence.device != self.device:
-            raise("Error: x_sequence tensor does not match device with Ordinet.")
+            raise ValueError("x_sequence tensor does not match device with Ordinet.")
         if loss_gradients.device != self.device:
-            raise("Error: loss_gradients tensor does not match device with Ordinet.")
+            raise ValueError("loss_gradients tensor does not match device with Ordinet.")
         if x_sequence.dtype != self.dtype:
-            raise("Error: x_sequence tensor does not match device with Ordinet.")
+            raise ValueError("x_sequence tensor does not match device with Ordinet.")
         if loss_gradients.dtype != self.dtype:
-            raise("Error: loss_gradients tensor does not match device with Ordinet.")
+            raise ValueError("loss_gradients tensor does not match device with Ordinet.")
 
         # the length of the sequence we are training on
         sequence_length = x_sequence.size(dim=0)
 
         # initialize the leading net activations
-        leading_activations = torch.zeros((self.container_size[0]-1, self.container_size[1], self.container_size[2]),
-                                            dtype=self.dtype, device=self.device) # leading = trailing for last layer
+        leading_activations = torch.zeros(self.container_size, dtype=self.dtype, device=self.device)
+
         # initialize gradient-calculating per-transfer values
         trailing_activations = torch.zeros(self.container_size, dtype=self.dtype, device=self.device)
+        # offset per-transfer values for calculating pole gradients
         offset_activations = torch.zeros(self.container_size, dtype=self.dtype, device=self.device)
 
         # keep track of the backwards loss dependencies
@@ -129,44 +133,86 @@ class Ordinet:
 
         # calculate the poles for each gamma
         poles = torch.log(self.gammas)
+
         # find the maximum forward-search depth
         max_depth = math.floor((-1/INTEGRATOR_LIMIT) * max_tau)
-        # find the forward-search depth of each transfer
-        search_depths = torch.floor(max_tau*torch.div(torch.full(self.container_size, 1.0, dtype=self.dtype, device=self.device), -1*poles))
 
-        # contain the layer-wise inputs generated by the leading activations
-        input_trails = [None for _ in range(self.num_layers-3)]
-        for l in range(self.num_layers-3):
-            input_trails[l] = torch.zeros([max_depth*(self.num_layers-3-l), self.layer_size], dtype=self.dtype, device=self.device)
+        # find the forward-search depth of each transfer ()
+        search_indexes = torch.floor(max_tau*torch.div(torch.full(self.container_size, 1.0, dtype=self.dtype, device=self.device), -1*poles))
+        search_indexes = search_indexes.type(dtype=torch.int32)
+
+        # find the maximum decay level at the given transfer search depth
+        max_decays = torch.exp(torch.mul(poles, search_indexes))
+
+        # search indexes must have the correct row offset for reshaped indexing
+        for l in range(search_indexes.size(dim=0)):
+            temp_search = search_indexes[l].reshape([search_indexes[l].nelement()])
+            temp_search += torch.concat([torch.full([search_indexes[l].size(dim=1)], i*search_indexes[l].size(dim=1), device=self.device) for i in range(search_indexes[l].size(dim=0))])
+            print(search_indexes[l])
+
+        # the distance between the trailing layer and leading layer at the index
+        tail_lengths = [k * max_depth for k in range(self.container_size[0], 0, -1)]
+
+        # contain the layer-wise inputs generated by the leading activations (needed for every intermediate layer)
+        input_trails = [None for _ in range(self.container_size[0])]
+        for l in range(1, self.num_hidden+1):
+            input_trails[l] = torch.zeros([tail_lengths[l], self.layer_size], dtype=self.dtype, device=self.device)
         # point to where we are in the trail
-        input_trail_indexes = [0 for _ in range(self.num_layers-3)]
+        input_trail_indexes = [0 for _ in range(self.container_size[0])]
 
         # contain the layer-wise loss gradients along the bottom staircase
-        loss_trails  = [None for _ in range(self.num_layers-3)]
-        for l in range(self.num_layers-3):
+        loss_trails  = [None for _ in range(self.container_size[0])]
+        for l in range(1, self.num_hidden+1):
             loss_trails[l] = torch.zeros([max_depth, self.layer_size], dtype=self.dtype, device=self.device)
         # point to where we are in the trail
-        loss_trail_indexes = [0 for _ in range(self.num_layers-3)]
-
-        # TODO: last layer must accumulate loss before leading edge begins moving forward
+        loss_trail_indexes = [0 for _ in range(self.container_size[0])]
 
         # iterate forward through the dataset
-        for t in range(sequence_length + (self.num_layers-3)*max_depth ):
+        for t in range(sequence_length + max(tail_lengths)):
             
+            # do the leading forward pass (only if font is within data sequence)
             if t < sequence_length:
-                # do the leading forward pass
+                
+                # get the input at t
                 input_leader = x_sequence[t].clone()
-                for b in range(self.container_size[0]-1):
+
+                # do a forward pass
+                for b in range(self.container_size[0]):
                     leading_activations[b] = torch.mul(leading_activations[b], self.gammas[b])
                     leading_activations[b] = torch.add(leading_activations[b], torch.mul(self.gains[b], torch.t(input_leader)))
 
-                    input_leader = torch.sum(leading_activations[b], dim=1)
-                    input_leader = self.act_func(input_leader)
+                    # we don't care about the final output TODO: take into account last activation gradient for loss
+                    if b < self.container_size[0]-1:
+                        input_leader = torch.sum(leading_activations[b], dim=1)
+                        input_leader = self.act_func(input_leader)
 
-                    # if this layer must store, then store
-                    if b < self.num_layers-3:
-                        input_trails[input_trail_indexes[b]] = input_leader.clone()
-            
-                # this input leader can be used for the final transfer input
+                        # this should always evaluate to a hidden layer, so save the input to the trail
+                        input_trails[b+1][input_trail_indexes[b]] = input_leader.clone()
 
-                
+            # every transfer must accumulate the loss in front of it
+            for l in range(self.container_size[0]):
+
+                new_loss = None
+
+                # only do this if the loss we are looking at is within the data sequence
+                if t - tail_lengths[l] + max_depth >= 0 and t - tail_lengths[l] < sequence_length:
+
+                    # if this is the last transfer then treat it a little different
+                    if l < self.container_size[0]-1:
+
+                        # grab the loss value that is being added to the convolution
+                        reshaped_search = search_indexes[l].reshape([search_indexes[l].nelement()]) # turn the search tensor to 1D
+                        new_loss = torch.index_select(loss_trails[l+1].reshape(loss_trails[l+1].nelement()), 0, reshaped_search).reshape(search_indexes[l].shape) # get the loss at the index and convert back to shape
+        
+                    else:
+
+                        # grab the loss from the loss gradient vector directly (assumes zero padding)
+                        reshaped_search = search_indexes[l].reshape([search_indexes[l].nelement()]) # turn the search tensor to 1D
+                        new_loss = torch.index_select(loss_gradients.reshape(loss_gradients[l+1].nelement()), 0, reshaped_search).reshape(search_indexes[l].shape) # get the loss at the index and convert back to shape
+
+                # otherwise just pad with zeroes
+                else:
+                    new_loss = torch.zeros(search_indexes[l].shape())
+
+                # add this new loss to the loss convolutions
+                loss_convolutions[l] += torch.mul(max_decays[l], new_loss)
